07941f5e833b98482526cf802113d0724c50851a
Sigfrid Dusci <sig@farmatotum.com>
2016-03-18 16:40:42 +0800

    cleanup DB schema

--- db/migrate/20160303003140_add_flow_to_events.rb
+++
@@ -1,6 +0,0 @@
-class AddFlowToEvents < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :events, :flow
-    add_foreign_key :events, :flows
-  end
-end
--- db/migrate/20160303003777_add_event_to_tags.rb
+++
@@ -1,6 +0,0 @@
-class AddEventToTags < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :tags, :event
-    add_foreign_key :tags, :events
-  end
-end
