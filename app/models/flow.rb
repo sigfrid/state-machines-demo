8bd5c70ed2e5a4c68b7ceb2d25f641a872611c0b
Sigfrid Dusci <sig@farmatotum.com>
2016-03-18 16:53:24 +0800

    add state machine template

--- app/models/flow.rb
+++ app/models/flow.rb
@@ -1,5 +1,14 @@
 class Flow < ApplicationRecord
   has_paper_trail class_name: 'FlowVersion'
 
+  #state_machine :state, initial: :saved do
 
+    #event :edit do
+      #transition [:saved, :edited] => :edited
+    #end
+
+    #event :hide do
+      #transition [:saved, :edited] => :hidden
+    #end
+  #end
 end
